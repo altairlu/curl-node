be0f061fb326e3ec19962f8e40a76f7b5e13abaa
MoLi <2560263173@qq.com>
2017-10-24 17:22:13 +0800

    shell中显示下载进度

--- test.js
+++ test.js
@@ -1,19 +1,6 @@
-const url = require('url')
-const {URL} = url
-const urlString = "https://user:pass@www.baidu.com:8080/search?qqq=123&yyy=true#hash"
+process.stdout.write('123')
+process.stdout.write('123')
 
-const test = [
-  "https://user:pass@www.baidu.com:8080/search?qqq=123&yyy=true#hash",
-  "http://www.baidu.com",
-  "http://www.baidu.com/"
-]
-const fields = ["href", "protocol", "username", "password", "hostname", "port", "pathname", "search", "hash"]
-const REG = /^(?:([A-Za-z]+):)?(?:\/{0,3})(?:([A-Za-z0-9]+)?:([A-Za-z0-9]+)@)?([\dA-Za-z.\-]+)(?:\:(\d+))?(\/[^?#]*)?(\?[^#]+)?(?:#(.*))?$/
-const blank = '        '
-test.forEach(url=>{
-  result = REG.exec(url)
-  fields.forEach((field, i)=>{
-    console.log(`${field}:${blank.substr(field.length)}${result[i]}`)
-  })
-  console.log('=========')
-})
+let backspace = Array(3).fill('\b').join('')
+process.stdout.write(backspace)
+process.stdout.write('12345')
